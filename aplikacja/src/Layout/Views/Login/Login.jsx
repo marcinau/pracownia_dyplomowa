11666bdc09a2842c108977ed3ff596e76ca7ffa7
marcin <augmarcinaug@gmail.com>
2019-10-30 10:55:24 +0100

    add login page, set project structure

---
+++ aplikacja/src/Layout/Views/Login/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import '../../Style/Login/Login.css'
+
+const Login = props => {
+    return (
+        <div className="LoginBody">
+            <div className="LoginBacgkroundImage">k</div>
+            <div className="LoginContainer">
+                <div className="LoginTitle">Logowanie</div>
+                <div className="LoginContent">
+                    <div className="LoginInputs"><label>dd</label><input /></div>
+                    <div className="LoginInputs"><label>dd</label><input /></div>
+                    <button>Zaloguj</button><button>Załóż konto</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
